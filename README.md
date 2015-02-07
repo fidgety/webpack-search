cb275d342cabc313464a4e0133ef45e683db27f8
Matthew Beasley <matthew.beasley@gmail.com>
2015-02-07 01:16:11 +0000

    add a server - you'll need an app id

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 A first attempt at some react and webpack, shall iterate it to something I'm happier with.
+
+npm i
+
+node server
+
+webpack --watch --colors
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var rp = require('request-promise');
+
+var app = express();
+
+
+app.use(express.static(__dirname + '/app'));
+
+app.get('/search/map/:maxlat/:maxlng/:minlat/:minlng', function (req, res) {
+    console.log('https://api.laterooms.com/search/map/max/' + req.params.maxlat + ',' + req.params.maxlng + '/min/' + req.params.minlat + ',' + req.params.minlng + '/')
+    rp.get({
+        url: 'https://api.laterooms.com/search/map/max/' + req.params.maxlat + ',' + req.params.maxlng + '/min/' + req.params.minlat + ',' + req.params.minlng + '/',
+        headers: {
+            'TLRG-AppId': 'IM NOT PUTTING THIS PUBLIC'
+        },
+        json: true
+    })
+        .then(function (data) {
+            res.json(data);
+        })
+        .catch(function (e) {
+            res.send('badness happened', e);
+        });
+});
+
+var server = app.listen(3004);
